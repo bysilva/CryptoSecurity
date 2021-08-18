60b49e5b4c302f90b1d9f5878004d385d5bbe5db
Edgar eduardo <eepsmr48@hotmail.com>
2021-08-18 14:21:18 -0500

    Add project files.

---
+++ CryptoSecurityOS/MainWindow.xaml.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CryptoSecurityOS
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        string key;
+        private void btnEncriptar_Click(object sender, RoutedEventArgs e)
+        {
+            key = txtKey.Text;
+            txtEncriptado.Text = Encriptar(txtTexto.Text);
+        }
+
+        private void btnDesencriptar_Click(object sender, RoutedEventArgs e)
+        {
+            key = txtKey.Text;
+            txtDesencriptado.Text = Desencriptar(txtTexto.Text);
+        }
+
+
+
+
+        public string Encriptar(string texto)
+        {
+            //arreglo de bytes donde guardaremos la llave
+
+            byte[] keyArray;
+
+            //arreglo de bytes donde guardaremos el texto que vamos a encriptar
+
+            byte[] Arreglo_a_Cifrar = UTF8Encoding.UTF8.GetBytes(texto);
+
+            //se utilizan las clases de encriptación
+            //provistas por el Framework
+            //Algoritmo MD5
+
+            MD5CryptoServiceProvider hashmd5 = new MD5CryptoServiceProvider();
+
+            //se guarda la llave para que se le realice
+            //hashing
+            keyArray = hashmd5.ComputeHash(
+            UTF8Encoding.UTF8.GetBytes(key));
+
+            hashmd5.Clear();
+
+            //Algoritmo 3DAS
+            TripleDESCryptoServiceProvider tdes = new TripleDESCryptoServiceProvider();
+
+            tdes.Key = keyArray;
+            tdes.Mode = CipherMode.ECB;
+            tdes.Padding = PaddingMode.PKCS7;
+
+            //se empieza con la transformación de la cadena
+            ICryptoTransform cTransform =
+            tdes.CreateEncryptor();
+
+            //arreglo de bytes donde se guarda la
+            //cadena cifrada
+            byte[] ArrayResultado =
+            cTransform.TransformFinalBlock(Arreglo_a_Cifrar,
+            0, Arreglo_a_Cifrar.Length);
+
+            tdes.Clear();
+
+            //se regresa el resultado en forma de una cadena
+            return Convert.ToBase64String(ArrayResultado,
+            0, ArrayResultado.Length);
+        }
+
+
+        public string Desencriptar(string textoEncriptado)
+        {
+            byte[] keyArray;
+            //convierte el texto en una secuencia de bytes
+            byte[] Array_a_Descifrar =
+            Convert.FromBase64String(textoEncriptado);
+
+            //se llama a las clases que tienen los algoritmos
+            //de encriptación se le aplica hashing
+            //algoritmo MD5
+            MD5CryptoServiceProvider hashmd5 =
+            new MD5CryptoServiceProvider();
+
+            keyArray = hashmd5.ComputeHash(
+            UTF8Encoding.UTF8.GetBytes(key));
+
+            hashmd5.Clear();
+
+            TripleDESCryptoServiceProvider tdes =
+            new TripleDESCryptoServiceProvider();
+
+            tdes.Key = keyArray;
+            tdes.Mode = CipherMode.ECB;
+            tdes.Padding = PaddingMode.PKCS7;
+
+            ICryptoTransform cTransform =
+            tdes.CreateDecryptor();
+
+            byte[] resultArray =
+            cTransform.TransformFinalBlock(Array_a_Descifrar,
+            0, Array_a_Descifrar.Length);
+
+            tdes.Clear();
+            //se regresa en forma de cadena
+            return UTF8Encoding.UTF8.GetString(resultArray);
+        }
+    }
+}
